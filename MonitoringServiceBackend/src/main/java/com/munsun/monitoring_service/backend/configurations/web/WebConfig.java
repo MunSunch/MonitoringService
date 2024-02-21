bf4926773e8db69568c2a986f23f398ecc5c4179
MunirTheBest <msunchalyaev@gmail.com>
2024-02-21 19:27:02 +0300

    update WebConfig

--- MonitoringServiceBackend/src/main/java/com/munsun/monitoring_service/backend/configurations/web/WebConfig.java
+++ MonitoringServiceBackend/src/main/java/com/munsun/monitoring_service/backend/configurations/web/WebConfig.java
@@ -1,8 +1,6 @@
 package com.munsun.monitoring_service.backend.configurations.web;
 
-import com.munsun.monitoring_service.commons.configurations.YamlPropertySourceFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
